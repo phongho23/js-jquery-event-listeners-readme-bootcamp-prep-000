30016651656f022702da4795b2e9be7c53c0817c
Phong Ho <ho.phongt@gmail.com>
2018-04-05 03:34:28 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,48 @@
 //define functions here
 
+function getIt(){
+  $('p').on("click", 
+  function clickP() {
+    alert("Hey!");
+  }
+    );
+}
+
+function frameIt(){
+  $('img').on("load", 
+  function loadImg() {
+    $('img').addClass('tasty');
+    return;
+  }
+    );
+}
+
+function pressIt(){
+  $('form').on("keydown", 
+  function keydownAlert(key) {
+  if (key.which === 71) {
+    alert("You pressed G!");
+    return;
+  }}
+    );
+}
+
+function submitIt(){
+  $('form').on("submit", 
+  function submitAlert() {
+    alert("Your form is going to be submitted now.");
+    return;
+  }
+    );
+}
+
+
 $(document).ready(function(){
 
-// call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
+
